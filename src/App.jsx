9b0b8de0c37300a312a416701c307eac455b376d
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-05-05 01:40:15 -0400

    Wrote function to set displayed answer based on button click

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,30 @@
 import './App.css'
+import { useState } from 'react';
 import data from './data';
 
 function App() {
+  const [displayedAnswer, setDisplayedAnswer] = useState(null);
+
+  function handleExpand(questionId) {
+    if (questionId === displayedAnswer) {
+      setDisplayedAnswer(null);
+    } else {
+      setDisplayedAnswer(questionId);  
+    } 
+  }
 
   return (
     <div>
       <h2>Questions and Answers About Login</h2>
-      <div>
         {data.map((item) => 
           <div key={item.id}>
-            <p>{item.title}</p>
+            <div className="question-section">
+              <p>{item.title}</p>
+              <button onClick={() => handleExpand(item.id)}>+</button>
+            </div>
             <p>{item.info}</p>
           </div>
         )}
-      </div>
     </div>
   )
 }
