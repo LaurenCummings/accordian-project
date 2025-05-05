6803a128daaaf88bf163e83252af9e867417c4ff
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-05-05 01:30:05 -0400

    mapped questions and answers

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import './App.css'
+import data from './data';
 
 function App() {
 
   return (
     <div>
-      
+      <h2>Questions and Answers About Login</h2>
+      <div>
+        {data.map((item) => 
+          <div key={item.id}>
+            <p>{item.title}</p>
+            <p>{item.info}</p>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
