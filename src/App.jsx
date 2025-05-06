a3e61fe38da8baaa3205395c2a002e5c819eddda
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-05-06 02:15:51 -0400

    Added box shadow around active q and a

--- src/App.jsx
+++ src/App.jsx
@@ -19,21 +19,23 @@ function App() {
       <h2>Questions and answers about login</h2>
       <div className="q-and-a-section">
         {data.map((item) => 
-          <div key={item.id}>
-            <div className="question-section">
-              <p>{item.title}</p>
-              <button 
-                onClick={() => handleExpand(item.id)}>
-                  { item.id == displayedAnswer 
-                    ? <FaMinus className="icon" /> 
-                    : <FaPlus className="icon" /> }
-              </button>
-            </div>
-            <div className="answer-section">
-              { item.id == displayedAnswer ? 
-                <p>{item.info}</p> 
-                : null 
-              }
+          <div className={item.id === displayedAnswer ? 'active' : 'inactive'}>
+            <div key={item.id}>
+              <div className="question-section">
+                <p>{item.title}</p>
+                <button 
+                  onClick={() => handleExpand(item.id)}>
+                    { item.id == displayedAnswer 
+                      ? <FaMinus className="icon" /> 
+                      : <FaPlus className="icon" /> }
+                </button>
+              </div>
+              <div className="answer-section">
+                { item.id == displayedAnswer ? 
+                  <p>{item.info}</p> 
+                  : null 
+                }
+              </div>
             </div>
           </div>
         )}
